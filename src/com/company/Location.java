785a3978bd87836e72427a2e2aaa95a219699da7
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-11-07 19:55:34 +0200

    made the Location class immutable - made changes in Main accordingly fixed issue with null initialization

--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -8,15 +8,15 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exists;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String,Integer> exists) {
         this.locationID = locationID;
         this.description = description;
-        this.exists = new HashMap<>();
+        if (exists!= null) {
+            this.exists = new HashMap<>(exists);
+        } else {
+            this.exists = new HashMap<>();
+        }
         this.exists.put("Q", 0);
-    }
-
-    public void addExit (String direction, int location) {
-        exists.put(direction, location);
     }
 
     public int getLocationID() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,27 +20,33 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "in from of a computer (0)"));
-        locations.put(1, new Location(1, "at the end of a road (1)"));
-        locations.put(2, new Location(2, "at the top of a hill (2)"));
-        locations.put(3, new Location(3, "inside of a building (3)"));
-        locations.put(4, new Location(4, "in a valley, near a stream (4)"));
-        locations.put(5, new Location(5, "in the forest (5)"));
+        Map <String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "in from of a computer (0)", null));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        tempExit = new HashMap<>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "at the end of a road (1)", tempExit));
 
-        locations.get(2).addExit("N", 5);
+        tempExit = new HashMap<>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "at the top of a hill (2)", tempExit));
 
-        locations.get(3).addExit("W", 1);
+        tempExit = new HashMap<>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "inside of a building (3)", tempExit));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tempExit = new HashMap<>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "in a valley, near a stream (4)", tempExit));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tempExit = new HashMap<>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "in the forest (5)", tempExit));
 
         Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
